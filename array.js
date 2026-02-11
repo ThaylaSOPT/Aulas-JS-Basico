be4152c654b4fc488016c2fc2a62ec6be4b7a8a7
ThaylaSOPT <thaylasop.trindade@gmail.com>
2026-02-11 14:55:35 -0300

    Aula 10.02

---
+++ array.js
@@ -0,0 +1,27 @@
+//let predio = [];
+
+//predio [0] = "Pedro";
+//predio [1] = "Maria";
+//predio [2] = "Fernando";
+//predio [3] = "Felipe";
+
+//console.log (predio);
+//console.log (predio[2]);
+
+//let numeros = [1,2,3,4,5,6,7,8,9,10];
+
+//console.log(numeros[10]);
+
+//let lista = new Array();
+//lista.push ("Abacaxi");
+//lista.push ("Banana");
+
+//console.log (lista);
+
+let teste = [];
+
+for (let i = 0; i < 10; i++){
+    teste.push(i + 1);
+}
+
+console.log (teste);
