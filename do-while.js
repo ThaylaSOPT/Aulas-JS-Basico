88ba2aedb072c54d8300522f0b9bd7b18213a555
ThaylaSOPT <thaylasop.trindade@gmail.com>
2026-02-11 14:57:32 -0300

    Aula 10.02 - II

---
+++ do-while.js
@@ -0,0 +1,9 @@
+let i = 0;
+
+while(i>0){
+    console.log("i>0");
+}
+
+do {
+    console.log("do while");
+} while (i<0);
---
+++ while.js
@@ -0,0 +1,14 @@
+//let i = 0;
+
+//while(i<10){
+    //console.log(i);
+    //i++;
+//}
+
+let password = "abc123";
+
+while(password != "abc123"){
+    console.log ("Senha incorreta");
+}
+
+console.log ("Senha corerta")
